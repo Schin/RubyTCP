92f299a6ec3105475efe362dbe7d2b7b1bc4d428
Ours <ours@ubuntu.(none)>
2013-02-17 01:25:57 +0100

    FAT server

--- chat_client.rb
+++ chat_client.rb
@@ -4,17 +4,47 @@ DOMAIN = "localhost"
 PORT = 2626
 TAILLE_TAMPON = 1000
 
-chat_client = TCPSocket.new(DOMAIN, PORT)
-
-fork do
-   first=true
-   loop do
-       puts chat_client.recvfrom(TAILLE_TAMPON)[0].chomp
-       print "Enter your username : " if first
-       first = false if first
-   end
+class DNC_Client
+	def initialize(domaine, port, username)
+		@chat_client = TCPSocket.new(domaine, port)
+		@chat_client.puts username
+	end
+	def run
+		@pid = fork do
+		   loop do
+		       res_mess = @chat_client.recvfrom(TAILLE_TAMPON)[0].chomp
+		       type, data = res_mess.split(" ",2)
+		       display(type, data)
+		   end
+		end
+		begin
+			while line = gets do
+				if line.match(/^\//) then
+      				@chat_client.puts "CMD "+line[1..-1].strip
+    			else
+    				@chat_client.puts "MSG "+line.strip
+				end
+			end
+		rescue Exception
+			stop
+		end
+	end
+	def stop
+		@chat_client.close
+		exit!(0)
+	end
+	def display(type, data)
+		case type
+		when "000"
+			puts data
+		when "111"
+			puts data
+		when "666"
+			puts data
+			stop
+		end
+	end
 end
-
-while line = gets do
-   chat_client.puts line
-end
+print "Enter your username: "
+username = gets.chomp
+client = DNC_Client.new(DOMAIN, PORT, username).run
--- chat_server.rb
+++ chat_server.rb
@@ -2,81 +2,117 @@ require 'socket'
 
 class DNCServer
 
- def initialize ( port )
-    @descriptors  = Array::new # array to keep track of the sockets that exist for the server
-    @clients      = Hash::new  # hash to keep all pseudo of the ips for the server
+  def initialize ( port )
+    @descriptors  = Array::new #Permet de stocker tous les sockets
+    @kennels      = Hash::new #Permet de garder toutes les Kennels
+    @clients      = Hash::new #Permet de gardes les pseudos des users et leur ip
     @serverSocket = TCPServer.new("", port)
     @serverSocket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1)
     puts "Dog Now Chat started on port #{port}"
     @descriptors.push( @serverSocket ) #add socket to descriptor array
- end
+  end
 
- # implements the chat functionality
- def run
+  # implements the chat functionality
+  def run
     trap(:SIGINT) {
-       broadcast_string("Server shuting down..." , @serverSocket)
-       puts "Server off"
-       @serverSocket.close
-       exit!(0)
-       }
-    while 1
-     res = select(@descriptors, nil, nil ,nil)
-     if res != nil then
-       #Iterate through the tagged read descriptors
-       for sock in res[0]
-         #Recieved a connect to the server (listening) socket
-         if sock == @serverSocket then
-             accept_new_connection
-         else
-           #Received something on a client socket
-           if sock.eof? then
-             print(sprintf("Client left %s: %s \n",
-                           sock.peeraddr[2], sock.peeraddr[1]))
-             ip = sock.peeraddr[2].to_s+"|"+sock.peeraddr[1].to_s
-             str = @clients[ip] + " left the server.\n"
-             broadcast_string( str, sock )
-             sock.close
-             @descriptors.delete(sock)
-           else
-             ip = sock.peeraddr[2].to_s+"|"+sock.peeraddr[1].to_s
-             str = sprintf("[%s]: %s", @clients[ip], sock.gets())
-             broadcast_string( str, sock )
-           end
-         end
-       end
-     end
+      stop
+    }
+    while true
+      res = select(@descriptors, nil, nil ,nil)
+      if res != nil then
+        #Iterate through the tagged read descriptors
+        for sock in res[0]
+          #Recieved a connect to the server (listening) socket
+          if sock == @serverSocket then
+            raise "ERROR " if not accept_new_connection
+          else
+          #Received something on a client socket
+            if sock.eof? then
+              print(sprintf("Client left %s: %s \n", sock.peeraddr[2], sock.peeraddr[1]))
+
+              ip  = sock.peeraddr[2].to_s+"|"+sock.peeraddr[1].to_s
+              str = @clients[ip] + " left the server.\n"
+
+              broadcast_string(str, sock)
+
+              sock.close
+
+              @descriptors.delete(sock)
+            else
+              ip      = sock.peeraddr[2].to_s+"|"+sock.peeraddr[1].to_s
+              
+              tram = sock.gets()
+              processing_tram(tram)
+              str     = sprintf("[%s]: %s", @clients[ip], message)
+
+              broadcast_string(str, sock)
+            end
+          end
+        end
+      end
     end
- end
+  end
 
-
- private #these methods are used for internally sending a message to all
-         #clients and accepting a new client connection
-
- def broadcast_string (str, omit_sock)
+  def stop
+    broadcast_string("666 Server shuting down..." , @serverSocket)
     @descriptors.each do |clisock|
-     if clisock != @serverSocket && clisock != omit_sock
-       clisock.write(str)
-     end
+      if clisock != @serverSocket
+        clisock.close
+      end
+    end
+    @serverSocket.close
+    puts "Server off"
+    exit!(0)
+  end
+
+  private
+
+  def processing_string (str)
+  end
+
+  def broadcast_string (str, omit_sock)
+    @descriptors.each do |clisock|
+      if clisock != @serverSocket && clisock != omit_sock
+        clisock.write(str)
+      end
     end
     print("CLIENT : " + str)
- end
+  end
 
- def accept_new_connection
+  def accept_new_connection
     newsock = @serverSocket.accept
-   
-    newsock.write("You're connected to X's personal chatserver\n")
+
+    newsock.write("000 You're connected to Dog Now Chat server\n")
     print(sprintf("Client joined %s:%s\n", newsock.peeraddr[2], newsock.peeraddr[1]))
-    ip = newsock.peeraddr[2].to_s+"|"+newsock.peeraddr[1].to_s
-    begin
-     @clients[ip] = newsock.gets().chomp
-    rescue Exception => e
-     return false
-    end
+
+    ip  = newsock.peeraddr[2].to_s+"|"+newsock.peeraddr[1].to_s
     str = @clients[ip] + " joined the server.\n"
+
+    begin
+      @clients[ip] = newsock.gets().chomp
+    rescue Exception
+      return false
+    end
+
     @descriptors.push(newsock)
     broadcast_string( str, newsock )
- end
 
+    return true
+  end
+  class User
+    def initialize(name, socket)
+      @name   = name
+      @ip     = socket.peeraddr[2].to_s
+      @port   = socket.peeraddr[1].to_s
+      @socket = socket
+    end
+  end
+  class Kennel
+    def initialize(name)
+      @name = name
+      @users = Array::new
+    end
+  end
 end
 
 
