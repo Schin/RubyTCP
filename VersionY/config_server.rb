02401536036ada8f0712b8f35f433a13de4c45c6
Schin <david.eglem@gmail.com>
2013-02-23 22:10:12 +0100

    version folder

---
+++ VersionY/config_server.rb
@@ -0,0 +1,9 @@
+#---------------INITIALIZE---------------#
+PORT = 2626
+DOMAIN = ""
+VERBOSE = false
+ERRORS = true
+TIMEOUT = 5
+START = "Dogs Now Chat started on port "
+#-----------------CODES-----------------#
+M666 = "Server shuting down..."
